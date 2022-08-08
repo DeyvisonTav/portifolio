b0e728c7169403305194dd94ed930dc9c58efe53
Deyvison Tavares <deyvisontav@gmail.com>
2022-08-08 14:53:29 -0300

    secret project

--- pages/secret.jsx
+++ pages/secret.jsx
@@ -17,7 +17,7 @@ const secret = () => {
           alt="/"
         />
         <div className="absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2">
-          <h2 className="py-2">Twitch UI</h2>
+          <h2 className="py-2">Secret Word</h2>
           <h3>React / Tailwind / ApiRest</h3>
         </div>
       </div>
